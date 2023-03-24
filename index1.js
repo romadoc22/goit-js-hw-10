4304eef60af864054d8163576a9abf986179b1c7
romadoc22 <113574669+romadoc22@users.noreply.github.com>
2023-03-24 18:31:30 +0200

    переніс функцію в окремий файл

--- index1.js
+++ index1.js
@@ -1,10 +1,11 @@
-fetch(
-  'https://restcountries.com/v3.1/name/ukraine?fields=name,flags,capital,population,languages'
-)
-  .then(response => {
+export function fetchCountries(name) {
+  const START_URL = 'https://restcountries.com/v3.1/name';
+  const FILTER_OPTION = '?fields=name,capital,population,flags,languages';
+  const url = `${START_URL}/${name}${FILTER_OPTION}`;
+  return fetch(url).then(response => {
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
     return response.json();
-  })
-  .then(country => {
-    console.log(country);
-    const markUp = countryCard(country);
   });
+}
--- src/index.js
+++ src/index.js
@@ -1,18 +1,7 @@
 import './css/styles.css';
 import debounce from 'lodash.debounce';
 import Notiflix from 'notiflix/build/notiflix-notify-aio';
-
-function fetchCountries(name) {
-  const START_URL = 'https://restcountries.com/v3.1/name';
-  const FILTER_OPTION = '?fields=name,capital,population,flags,languages';
-  const url = `${START_URL}/${name}${FILTER_OPTION}`;
-  return fetch(url).then(response => {
-    if (!response.ok) {
-      throw new Error(response.status);
-    }
-    return response.json();
-  });
-}
+import { fetchCountries } from '../index1';
 
 const searchBox = document.querySelector('#search-box');
 const countriesList = document.querySelector('.country-list');
